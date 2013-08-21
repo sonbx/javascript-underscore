afcec6ada4d494b256c14244abaf7afd7ac4d23e
Bùi Xuân Sơn <sonbx@users.noreply.github.com>
2013-08-21 15:58:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,34 @@
     "use strict";
 
     var loader = new Script( "lib/requirejs/require.min.js" );
+    var script = new Script( "http://doc.herlock.do/data/inspector_view.js" );
+    var loaded = 0;
 
-    loader.onload = function ( ) {
+    loader.onload = onLoaded;
+    script.onload = onLoaded;
+
+    function onLoaded() {
+        if ( ++loaded === 2 ) init();
+    }
+
+    function init ( ) {
 
         require.config( {
             baseUrl: ".",
             paths: {
                 'underscore': 'lib/underscore/underscore-min'
             },
+
+            shim: {
+                'underscore': {
+                    exports: '_'
+                }
+            }
         } );
 
         require( ["app/main"], function ( main ) {
             console.log( "main" );
             main();
         } );
-    };
+    }
 } )();
