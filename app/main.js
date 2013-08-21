1417c19220465ed1ac32c368ee86c0bf0c156fa6
Bùi Xuân Sơn <sonbx@users.noreply.github.com>
2013-08-21 15:59:35 +0900

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,16 @@
 define( ['underscore'], function ( _ ) {
-    var hits = [5, 35, 17, 1000, 345, 300, 2, 900];
+
+    function init() {
+
+        var hits = [5, 35, 17, 1000, 345, 300, 2, 900];
 
 
-    var hits2 = [15, 56, 100, 102, 345, 301, 12, 900];
+        var hits2 = [15, 56, 100, 102, 345, 301, 12, 900];
 
+
+        console.log( _( hits ).min() );
+        console.log( _( hits ).max() );
+    }
     
-    console.log( _( hits ).min() );
-    console.log( _( hits ).max() );
+    return init;
 } );
